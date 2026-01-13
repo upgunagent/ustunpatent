82f358f4b04d2b386a515a19b4d3186a48a93a0d
Ozgur Topkan <upgunagent@gmail.com>
2026-01-13 15:35:05 +0300

    Initial commit: Next.js + Tailwind + Basic Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "UstunPatent CRM",
+  description: "Advanced CRM solution for UstunPatent",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
